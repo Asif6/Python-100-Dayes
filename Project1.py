7db97d972e636ac4f683352979dd8f9ff1194c21
= <=>
2023-01-26 02:28:59 +0600

    Project 1 Band Name Generator

---
+++ Project1.py
@@ -0,0 +1,33 @@
+print("Day 1- Python Print Function")
+print("The function is declared like this:")
+print("print( 'what to print' )")
+print("Day 1- Python Print Function\nThe functio is declared like this:\nprint( 'what to print' )")
+print("Asif"+" "+"Foysal")
+input("What is your name: ") # This line take input from user
+
+print("Hello " + input("What is your name?")) # This line take the input first then print the output 
+
+print( len( input( "What is your name? ") ) ) # The len() function for calculating the length of a string
+
+Name=input("What is your name? ") # Variables in python 
+print("My name is " + Name)
+
+# How to switch valu betwine Two variable
+
+A=input("A= ") # Take input from user for A
+B=input("B= ") # Take input from user for B
+
+A,B=B,A # Swaping valu betwine two variable
+
+print("A:", A) # Printing valu A
+print("B:", B) # Printing valu B
+
+# The final project for day 1 Project Name= "The Band name generator"
+
+print("Welcome to the Band Name Generator.")
+
+User_city_name= input("What's name of the city you grew up in?\n") # User input
+User_pet_name= input("What's your pet's name?\n")
+print("Your band name could be" +" "+User_city_name + " " + User_pet_name) # Final output
+
+
